87b8fb5412e15a08d368d8899f660459ad8393a9
riqueschilder <99187952+riqueschilder@users.noreply.github.com>
2022-02-26 22:57:09 -0300

    Mapa novo mais carro

---
+++ src/Godot/World/Carros/Carro.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+onready var animationSprite = $AnimatedSprite
+onready var animations = []
+onready var azul = "DireitaAzul"
+onready var ciano = "DireitaCiano"
+onready var verde = "DireitaVerde"
+onready var laranja = "DireitaLaranja"
+onready var vermelho = "DireitaVermelho"
+onready var cinza = "DireitaCinza"
+
+var a = ((randi() % 3) + 2 )
+var i = 0
+func _ready():
+	animations.append(azul)
+	animations.append(verde)
+	animations.append(ciano)
+	animations.append(laranja)
+	animations.append(vermelho)
+	animations.append(cinza)
+	animations.shuffle()
+	
+func chegouLimite():
+		if (position.x > 1295):
+			position.x = -465
+			a = ((randi() % 3) + 2 )
+			randomize()
+			animations.shuffle()
+	
+func _process(delta):
+	
+	animationSprite.play(animations[i])
+	move_local_x(a)
+	chegouLimite()
+	
