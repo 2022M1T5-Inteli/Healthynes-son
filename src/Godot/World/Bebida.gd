cd84305b8f093342efb4bc59889a6e5070b5976f
VitorMoura01 <vitor.oliveira@sou.inteli.edu.br>
2022-03-15 15:07:15 -0300

    Added Code Commentaries
    IA Mãe

--- src/Godot/World/Bebida.gd
+++ src/Godot/World/Bebida.gd
@@ -7,7 +7,7 @@ func _physics_process(delta):
 		for body in bodies:
 			if body.name == "Player":
 				print("corpo sobre o copo")
-				get_tree().change_scene("MenuTask2.tscn")
+				get_tree().change_scene("res://World/Mini-games/Pacman/MoveMae/Level.tscn")
 				
 				if body.name != "Player":
 					print("corpo não esta sobre o copo")
