f69f624aa4c508cf3aca4457ec95138f4b79e36b
Gabriela Rodrigues <99189378+gabInteli@users.noreply.github.com>
2022-03-16 09:47:05 -0300

    update Cards and Doors

---
+++ src/Godot/Pacman/Cards/Card3.gd
@@ -0,0 +1,4 @@
+extends Area2D
+
+func _on_Button3_pressed():
+	get_tree().change_scene("res://Pacman/MoveMae/Level.tscn")
--- src/Godot/World/Mini-games/Pacman/Cards/Card1.gd
+++
@@ -1,4 +0,0 @@
-extends Area2D
-
-func _on_Button_pressed():
-	$Sprite1.visible = false
