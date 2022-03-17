0b8b5f653921ce3db0719e722e5a0aa67fcc138b
riqueschilder <99187952+riqueschilder@users.noreply.github.com>
2022-03-17 15:32:27 -0300

    musicas

--- src/Godot/Pacman/Pontuacao.gd
+++ src/Godot/Pacman/Pontuacao.gd
@@ -3,10 +3,15 @@ extends Node2D
 func _input(event):
 	print(tempo.pontos)
 	
-	
+func _ready():
+	$MrSandman.play()
 		
 func _process(delta):
+	if tempo.pontos < 2000:
+		$September.stop()
 	if tempo.pontos == 2000:
+		$September.play()
+		$MrSandman.stop()
 		get_tree().paused = true
 		visible = true
 	if tempo.player_dead:
@@ -15,10 +20,12 @@ func _process(delta):
 		tempo.player_dead = false
 	
 	if tempo.time <= 40:
+		$September.play()
 		$Sprite2.visible =true
 		$Sprite3.visible =true
 		$Sprite4.visible =true
-		
+	
+	
 	elif tempo.time <= 50:
 		$Sprite2.visible =true
 		$Sprite3.visible =true
@@ -42,6 +49,7 @@ func _process(delta):
 
 
 func _on_Button_pressed():
+	tempo.time = 0
 	tempo.timer_on = true
 	tempo.pontos = 0
 	get_tree().paused = false
