a76f2bd9f60d52e1a751042b29295c4170238635
Gabriela Rodrigues <99189378+gabInteli@users.noreply.github.com>
2022-03-20 14:18:27 -0300

    Update door

--- src/Godot/Door.gd
+++ src/Godot/Door.gd
@@ -3,13 +3,15 @@ extends Area2D
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	var overlapping = false
+	var timing = true
 	if bodies:
-		$AnimationPlayer.play("closed_door")
+		
 		for body in bodies:
 			if body.name == "Player":
 				$AnimationPlayer.play("idle")
+				yield(get_tree().create_timer(1.0), "timeout")
+				timing = false
 				get_tree().change_scene("res://World/House.tscn")
-				print("colisão aconteceu")
-				
+							
 				if body.name != "Player":
 					 $AnimationPlayer.play("closed_door")
