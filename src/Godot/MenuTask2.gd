d420996c7c7ffa64964a405393bb29bfabee74b1
Gabriela Rodrigues <99189378+gabInteli@users.noreply.github.com>
2022-03-28 14:52:44 -0300

    add marketTask

---
+++ src/Godot/MenuTask2.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+
+func _on_Jogar_pressed():
+	get_tree().change_scene("res://World/Mini-games/Trabalho/Level2.tscn")
+
+
+func _on_Voltar_pressed():
+	get_tree().change_scene("res://World/Work.tscn")
