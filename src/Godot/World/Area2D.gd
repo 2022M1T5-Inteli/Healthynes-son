c8dfa33c49d14f7ea7892d354a18543f851c2e94
riqueschilder <99187952+riqueschilder@users.noreply.github.com>
2022-04-01 08:06:48 -0300

    Saida trabalho e cards arrumados

--- src/Godot/World/Area2D.gd
+++ src/Godot/World/Area2D.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
 func _physics_process(delta):
-	var bodies = get_overlapping_bodies()
-	var overlapping = false
-	if bodies:
-		for body in bodies:
-			if body.name == "Player":
-				get_tree().change_scene("res://World.tscn")
-				print("colisão aconteceu")
+	if tempo.terminouCards:
+		var bodies = get_overlapping_bodies()
+		var overlapping = false
+		if bodies:
+			for body in bodies:
+				if body.name == "Player":
+					get_tree().change_scene("res://World.tscn")
+					print("colisão aconteceu")
 				
 				
