d79552b5508e418e07d2f2dd6376122591f12f23
riqueschilder <99187952+riqueschilder@users.noreply.github.com>
2022-04-01 08:35:26 -0300

    Revert "Saida trabalho e cards arrumados"
    This reverts commit c8dfa33c49d14f7ea7892d354a18543f851c2e94.

--- src/Godot/World/Area2D.gd
+++ src/Godot/World/Area2D.gd
@@ -1,13 +1,12 @@
 extends Area2D
 
 func _physics_process(delta):
-	if tempo.terminouCards:
-		var bodies = get_overlapping_bodies()
-		var overlapping = false
-		if bodies:
-			for body in bodies:
-				if body.name == "Player":
-					get_tree().change_scene("res://World.tscn")
-					print("colisão aconteceu")
+	var bodies = get_overlapping_bodies()
+	var overlapping = false
+	if bodies:
+		for body in bodies:
+			if body.name == "Player":
+				get_tree().change_scene("res://World.tscn")
+				print("colisão aconteceu")
 				
 				
