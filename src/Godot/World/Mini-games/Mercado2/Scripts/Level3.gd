4fe7b139bdc3394db2828ef1cef9ef2b7165bb67
riqueschilder <99187952+riqueschilder@users.noreply.github.com>
2022-04-13 10:02:56 -0300

    teste

--- src/Godot/World/Mini-games/Mercado2/Scripts/Level3.gd
+++ src/Godot/World/Mini-games/Mercado2/Scripts/Level3.gd
@@ -1,15 +1,10 @@
 extends Node2D
 
 func _ready():
-	$Sprite.visible = false
+
 	Global.pontos = 0
 
-func _physics_process(delta):
-	var timing = true 
-	yield(get_tree().create_timer(20.0), "timeout")
-	timing = false
-	$Sprite.visible = true 
-	get_tree().paused = true
+
 	 
 
 func _on_Button2_pressed():
--- src/Godot/World/Mini-games/Pacman/Pontuacao.gd
+++ src/Godot/World/Mini-games/Pacman/Pontuacao.gd
@@ -1,40 +1,33 @@
 extends Node2D
 
-func _input(event):
-	print(tempo.pontos) #debug
+
 	
 func _ready():
-	tempo.pontos = 0
-	tempo.time = 0
-#	pass
+	Global.pontos = 0
+
 	$MrSandman.play() #musica
-		
-func _process(delta):# sistema de estrelas e musicas
-	if tempo.pontos < 2100:
-		$September.stop()
-	if tempo.pontos == 2100: # musicas 
-		$September.play()
-		$MrSandman.stop()
-		get_tree().paused = true
-		visible = true
-	if tempo.player_dead:
-		tempo.pontos = 2100
-		tempo.time = 999999
-		$tempo.visible = false
-		tempo.player_dead = false
-	
-	if tempo.time <= 50: # sistema de estrelas, menor que 50 segundos 3 estrelas		
-		$September.play()
+	yield(get_tree().create_timer(60.0), "timeout") 
+	$September.play()
+	$MrSandman.stop()
+	get_tree().paused = true
+	visible = true
+func _process(delta):
+	if Global.pontos >= 3000: # sistema de estrelas, menor que 50 segundos 3 estrelas		
 		$Sprite2.visible =true
 		$Sprite3.visible =true
 		$Sprite4.visible =true
 
-	elif tempo.time <= 60: # sistema de estrelas, menos que 60 segundos 2 estrelas
+	elif Global.pontos >= 2000: # sistema de estrelas, menos que 60 segundos 2 estrelas
 		$Sprite2.visible =true
 		$Sprite3.visible =true
 		$Sprite4.visible = false
 		
-	if tempo.time > 60:# sistema de estrelas, maior que 60, 0 estrelas 
+	elif Global.pontos >= 1000: # sistema de estrelas, menos que 60 segundos 2 estrelas
+		$Sprite2.visible =true
+		$Sprite3.visible = false
+		$Sprite4.visible = false
+		
+	if Global.pontos < 100:# sistema de estrelas, maior que 60, 0 estrelas 
 		$Sprite2.visible = false
 		$Sprite3.visible = false
 		$Sprite4.visible = false
@@ -45,16 +38,16 @@ func _process(delta):# sistema de estrelas e musicas
 
 
 func _on_Button_pressed(): # sistema de reiniciar mini game
-	tempo.time = 0
-	tempo.timer_on = true
-	tempo.pontos = 0
+	
+	
+	Global.pontos = 0
 	get_tree().paused = false
 	get_tree().reload_current_scene()
-	yield(get_tree().create_timer(3.0), "timeout")
+	
 	
 	
 
 func _on_Button2_pressed():
-	get_tree().change_scene("res://World/Espacos/Casa/Scenes/Casa.tscn")
+	get_tree().change_scene("res://World/Espacos/Mercado/Scenes/Mercado.tscn")
 	get_tree().paused = false
 	Invent.pointsInv =+ 1
